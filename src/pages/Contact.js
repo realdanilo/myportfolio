ff85e9d813737c6e3b8cff39507e1c6295b7dda0
Danilo <realdanilo@users.noreply.github.com>
2020-11-11 15:30:34 -0700

    update profile info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from 'react-icons/ai'
+import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 import '../css/Contact.css'
 export default function Contact() {
     return (
@@ -7,7 +7,6 @@ export default function Contact() {
             <h3>You can find me at:</h3>
             <a rel="noopener noreferrer" href="https://github.com/realdanilo" target="_blank"><AiFillLinkedin /></a>
             <a rel="noopener noreferrer" href="https://github.com/realdanilo" target="_blank"><AiFillGithub /></a>
-            <a rel="noopener noreferrer" href="mailto:danilomera.10@gmail.com"><AiOutlineMail /></a>
         </div>
     )
 }
